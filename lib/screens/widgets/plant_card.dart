40f6ec4a819306d4469f91835a2c508502c8932d
uluhulu <58256177+uluhulu@users.noreply.github.com>
2020-10-28 13:50:08 +0500

    Добавида gesture detector

--- lib/screens/widgets/plant_card.dart
+++ lib/screens/widgets/plant_card.dart
@@ -9,72 +9,77 @@ class PlantCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          _buildPlantImage(plant.photoPath),
-          Padding(
-            padding: const EdgeInsets.only(bottom:175.0, right: 8.0),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: FloatingActionButton(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
-                mini: true,
-                heroTag: Container(),
-                onPressed: () {
-                  // Add your onPressed code here!
-                },
-                child: Icon(Icons.add, size: 28,),
+      child: GestureDetector(
+        onTap: (){
+          print('hello');
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Ink(child: _buildPlantImage(plant.photoPath)),
+            Padding(
+              padding: const EdgeInsets.only(bottom:175.0, right: 8.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  mini: true,
+                  heroTag: Container(),
+                  onPressed: () {
+                    // Add your onPressed code here!
+                  },
+                  child: Icon(Icons.add, size: 28,),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 180.0, right: 42),
-            child: Container(
-              decoration: BoxDecoration(color: Colors.white),
-              alignment: Alignment.bottomCenter,
-              height: 56,
-              width: 122,
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 8.0, left: 4.0),
-                      child: Text(
-                        plant.name,
-                        maxLines: 2,
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w700,
+            Padding(
+              padding: const EdgeInsets.only(top: 180.0, right: 42),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.white),
+                alignment: Alignment.bottomCenter,
+                height: 56,
+                width: 122,
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8.0, left: 4.0),
+                        child: Text(
+                          plant.name,
+                          maxLines: 2,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 2.0, left: 4.0),
-                      child: Text(
-                        plant.latName,
-                        maxLines: 2,
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 10.0,
-                          // fontWeight: FontWeight.w700,
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 2.0, left: 4.0),
+                        child: Text(
+                          plant.latName,
+                          maxLines: 2,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 10.0,
+                            // fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
