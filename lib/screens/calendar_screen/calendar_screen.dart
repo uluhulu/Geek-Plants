3bd4dddd300ca4ce41c964f382d3cf339f5b14f0
belenkov <belenkov@surfstudio.ru>
2020-12-12 09:48:47 +0500

    add calendar day events screen

---
+++ lib/screens/calendar_screen/calendar_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class CalendarScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _CalendarScreenState();
+  }
+
+}
+
+class _CalendarScreenState extends State{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
